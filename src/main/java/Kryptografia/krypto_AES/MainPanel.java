c322ff5fff45c6ee9a8f1510efae94e5ece38d95
m.szymczynska <m.a.szymczynska@accenture.com>
2016-04-02 15:24:59 +0200

    firts commit

---
+++ src/main/java/Kryptografia/krypto_AES/MainPanel.java
@@ -0,0 +1,78 @@
+package Kryptografia.krypto_AES;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+import java.awt.Color;
+import javax.swing.JTextArea;
+
+public class MainPanel extends JFrame{
+	public MainPanel() {
+		getContentPane().setLayout(null);
+		setSize(500, 500);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    try{
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	    
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 484, 462);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblWpiszTekstDo = new JLabel("Wpisz tekst do zaszyfrowania");
+		lblWpiszTekstDo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblWpiszTekstDo.setBounds(33, 11, 301, 29);
+		panel.add(lblWpiszTekstDo);
+		
+		JButton btnZaszyfruj = new JButton("Zaszyfruj");
+		btnZaszyfruj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			//szyfrowanie
+			}
+		});
+		
+		btnZaszyfruj.setBounds(33, 193, 89, 23);
+		panel.add(btnZaszyfruj);
+		
+		JLabel lblWynik = new JLabel("Wynik:");
+		lblWynik.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblWynik.setBounds(33, 233, 142, 23);
+		panel.add(lblWynik);
+		
+		JButton btnOdszyfruj = new JButton("Odszyfruj");
+		btnOdszyfruj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//odszyfrowanie
+			}
+		});
+		btnOdszyfruj.setBounds(149, 193, 89, 23);
+		panel.add(btnOdszyfruj);
+		
+		JLabel label = new JLabel("...");
+		label.setForeground(Color.BLACK);
+		label.setVerticalAlignment(SwingConstants.TOP);
+		label.setBounds(33, 267, 411, 151);
+		panel.add(label);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(33, 51, 411, 131);
+		panel.add(textArea);
+	}
+}
